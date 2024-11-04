9cad7cfa1997d3e9a8e5b59d96e9013d99e42333
Rohit <rohitmalakar007@gmail.com>
2024-11-04 17:43:54 +0530

    Demo update

--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
 import os 
 from secret import huggingface_key
 from langchain_huggingface import HuggingFaceEndpoint
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_core.output_parsers import StrOutputParser
 
 os.environ["HUGGINGFACEHUB_API_TOKEN"] = huggingface_key
 
@@ -8,7 +11,19 @@ llm_repo = "mistralai/Mistral-7B-Instruct-v0.3"
 
 llm = HuggingFaceEndpoint(repo_id=llm_repo, 
                           max_length = 128, 
-                          temperature=0.7, 
+                          temperature=0.4, 
                           token = huggingface_key)
-name = llm.invoke("What is the machine learning?")
-print(name)
+
+input = input("Enter any celebrity name...")
+# name = llm.invoke(input)
+
+
+#Prompt templates
+input_template = PromptTemplate(
+    input_variables = ['name'],
+    template = "Tell me about the celebrity {name} "
+)
+
+chain = input_template | llm 
+# chain = LLMChain(llm = llm, prompt = input_template, verbose = True)
+print(chain(input))
